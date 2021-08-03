62cceec2720fc97fa06a23b8482c9e714ffaf50a
DESKTOP-R71C5UD\fitz <zjtxchen@gmail.com>
2021-08-03 17:32:13 +0800

    整合gateway--未完成

---
+++ cloud-gateway-6666/src/main/java/com/bin/Gateway6666.java
@@ -0,0 +1,13 @@
+package com.bin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Gateway6666 {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway6666.class,args);
+    }
+}
